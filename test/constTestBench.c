583d1672fbdd07b1af984fa203e24f08297e0151
haotianmichael <2398419426@buaa.edu.cn>
2020-03-14 12:49:18 +0800

    测试系统@分别添加测试样例
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

---
+++ test/constTestBench.c
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+const char u = 'u', v = 'v', w = 'w';
+const char g = 'a';
+const int i = 1;
+const int a = 2, b = 3, c = 5;
---
+++ test/varTestBench.c
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+const int n = 1, m = 2;
+
+int a[2];
+int b[3], c[2], d[2];
+
+int g;
+int x, y, z;
+
+

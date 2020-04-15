813e8360f7b631510afd0e74b2a1b152d5da0a5e
haotianmichael <2398419426@buaa.edu.cn>
2020-04-15 21:29:49 +0800

    检查BUG:  表达式计算————错误状态机   [a-z][* | /]^*[a-z]
    Signed-off-by: haotianmichael <2398419426@buaa.edu.cn>

--- test/expressionTestBench.c
+++ test/expressionTestBench.c
@@ -28,5 +28,6 @@ main() {
     foo(a / -390 + c * -vara * 4);
     foo(-a + d[2]);
     foo(d[2] / -390 + d[3] * -vara * 4 + b[1]);
-    foo(bee(2 + 3) + d[3] * -vara + b[1] / -bee(d[3]));
+    foo(bee(2 + 3) + d[3] */ -vara +- b[1] / -bee(d[3]));
+
 }
